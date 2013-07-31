4bf489e09cb073276155a5ac2572dc65f4eeaa0b
Samuel Stauffer <samuelks@twitter.com>
2013-07-31 10:40:34 -0700

    Add GetDeviceName, GetDeviceUSBStrings, and GetIndexBySerial

--- rtl/rtl.go
+++ rtl/rtl.go
@@ -2,13 +2,59 @@
 package rtl
 
 // #cgo LDFLAGS: -lrtlsdr
+// #include <stdlib.h>
 // #include <rtl-sdr.h>
 import "C"
+
+import (
+	"errors"
+	"unsafe"
+)
+
+var (
+	ErrFailed           = errors.New("rtl: operation failed")
+	ErrNoDevices        = errors.New("rtl: no devices")
+	ErrNoMatchingDevice = errors.New("rtl: no matching device")
+)
 
 func GetDeviceCount() int {
 	return int(C.rtlsdr_get_device_count())
 }
 
-// RTLSDR_API uint32_t rtlsdr_get_device_count(void);
+func GetDeviceName(index int) string {
+	cName := C.rtlsdr_get_device_name(C.uint32_t(index))
+	return C.GoString(cName)
+}
 
-// RTLSDR_API const char* rtlsdr_get_device_name(uint32_t index);
+// Get USB device strings.
+// Return manufacturer, product name, and serial number
+func GetDeviceUSBStrings(index int) (string, string, string, error) {
+	var manufact [256]C.char
+	var product [256]C.char
+	var serial [256]C.char
+	if C.rtlsdr_get_device_usb_strings(C.uint32_t(index), (*C.char)(&manufact[0]), (*C.char)(&product[0]), (*C.char)(&serial[0])) != 0 {
+		return "", "", "", ErrFailed
+	}
+	return C.GoString((*C.char)(&manufact[0])), C.GoString((*C.char)(&product[0])), C.GoString((*C.char)(&serial[0])), nil
+}
+
+// Get device index by USB serial string descriptor.
+func GetIndexBySerial(serial string) (int, error) {
+	cSerial := C.CString(serial)
+	defer C.free(unsafe.Pointer(cSerial))
+	res := C.rtlsdr_get_index_by_serial(cSerial)
+	if res < 0 {
+		switch res {
+		case -1:
+			// Name is NULL. Shouldn't ever happen.
+			return -1, ErrFailed
+		case -2:
+			return -1, ErrNoDevices
+		case -3:
+			return -1, ErrNoMatchingDevice
+		default:
+			return -1, ErrFailed
+		}
+	}
+	return int(res), nil
+}
--- rtl/rtl_test.go
+++ rtl/rtl_test.go
@@ -5,9 +5,26 @@ import (
 )
 
 func TestBasics(t *testing.T) {
-	if devCount := GetDeviceCount(); devCount < 0 {
+	devCount := GetDeviceCount()
+	if devCount < 0 || devCount > 16 {
 		t.Fatal("GetDeviceCount failed")
-	} else {
-		t.Logf("Device count: %d", devCount)
+	}
+	t.Logf("Device count: %d", devCount)
+
+	for i := 0; i < devCount; i++ {
+		name := GetDeviceName(i)
+		if name == "" {
+			t.Fatalf("Failed to get device name for index %d", i)
+		}
+
+		manufact, product, serial, err := GetDeviceUSBStrings(i)
+		if err != nil {
+			t.Fatalf("GetDeviceUSBStrings failed: %+v", err)
+		}
+
+		t.Logf("Device %d: %s", i, name)
+		t.Logf("\tManufacturer: %s", manufact)
+		t.Logf("\tProduct: %s", product)
+		t.Logf("\tSerial: %s", serial)
 	}
 }
