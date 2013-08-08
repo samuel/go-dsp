8610079934554da7764a0cbf7f78e14f1a9605fb
Samuel Stauffer <samuel@descolada.com>
2013-08-08 01:04:38 -0700

    Fix ARM rotate90

--- sdr/util_arm.s
+++ sdr/util_arm.s
@@ -1,46 +1,46 @@
 
 TEXT ·rotate90FilterAsm(SB),7,$0
 	MOVW	samples_len+8(FP), R2
-	MOVW   	samples_ptr+4(FP), R5
+	MOVW	samples_ptr+4(FP), R5
 	AND	$(~3), R2	// round down to nearest multiple of 4
 	MOVW	$0, R3
 	B	loopStart
 loop:
-	ADD    	$4, R3
+	ADD	$4, R3
 loopStart:
-	CMP    	R3, R2
-	BLE    	loopEnd
+	CMP	R3, R2
+	BLE	loopEnd
 
 	MOVW	8(R5), R0
 	MOVW	12(R5), R1
 	EOR	$(1<<31), R1
-	MOVW	R1, 0(R5)
-	MOVW	R0, 4(R5)
+	MOVW	R1, 8(R5)
+	MOVW	R0, 12(R5)
 
 	MOVW	16(R5), R0
 	MOVW	20(R5), R1
 	EOR	$(1<<31), R0
 	EOR	$(1<<31), R1
-	MOVW	R0, 0(R5)
-	MOVW	R1, 4(R5)
+	MOVW	R0, 16(R5)
+	MOVW	R1, 20(R5)
 
 	MOVW	24(R5), R0
 	MOVW	28(R5), R1
 	EOR	$(1<<31), R0
-	MOVW	R1, 0(R5)
-	MOVW	R0, 4(R5)
+	MOVW	R1, 24(R5)
+	MOVW	R0, 28(R5)
 
 	ADD	$32, R5
 
-	B      	loop
+	B	loop
 loopEnd:
-	MOVW   	samples_ptr+4(FP), R0
-	MOVW   	R0, ret_ptr+16(FP)
-	MOVW   	samples_len+8(FP), R0
-	MOVW   	R0, ret_len+20(FP)
-	MOVW   	samples_cap+12(FP), R0
-	MOVW   	R0, ret_cap+24(FP)
-	MOVW   	$0, R0
-	MOVW   	R0, err+28(FP)
-	MOVW   	R0, err+32(FP)
+	MOVW	samples_ptr+4(FP), R0
+	MOVW	R0, ret_ptr+16(FP)
+	MOVW	samples_len+8(FP), R0
+	MOVW	R0, ret_len+20(FP)
+	MOVW	samples_cap+12(FP), R0
+	MOVW	R0, ret_cap+24(FP)
+	MOVW	$0, R0
+	MOVW	R0, err+28(FP)
+	MOVW	R0, err+32(FP)
 	RET
--- sdr/util_test.go
+++ sdr/util_test.go
@@ -4,7 +4,10 @@ import "testing"
 
 func TestRotate90Filter(t *testing.T) {
 	filter := &Rotate90Filter{}
-	input := []complex64{complex(0.0, 2.0), complex(1.0, 2.0), complex(-3.0, 7.0), complex(4.0, -9.0)}
+	input := make([]complex64, 256)
+	for i := 0; i < 256; i++ {
+		input[i] = complex(float32(i)-128.0, -(float32(i) - 128.0))
+	}
 	output, _ := rotate90FilterAsm(filter, input)
 	expected, _ := rotate90Filter(filter, input)
 	if len(output) != len(expected) {
