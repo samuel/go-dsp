1549b17bd02750d5fb06a2ee073dd7889584e31b
Samuel Stauffer <samuel@descolada.com>
2013-08-08 01:21:32 -0700

    Use bulk load/store for ARM

--- sdr/util_arm.s
+++ sdr/util_arm.s
@@ -1,36 +1,45 @@
 
 TEXT ·rotate90FilterAsm(SB),7,$0
-	MOVW	samples_len+8(FP), R2
-	MOVW	samples_ptr+4(FP), R5
-	AND	$(~3), R2	// round down to nearest multiple of 4
-	MOVW	$0, R3
+	MOVW	samples_len+8(FP), R7
+	MOVW	samples_ptr+4(FP), R8
+	AND	$(~3), R7	// round down to nearest multiple of 4
+	ADD	$8, R8
+	MOVW	$0, R6
 	B	loopStart
 loop:
-	ADD	$4, R3
+	ADD	$4, R6
 loopStart:
-	CMP	R3, R2
+	CMP	R6, R7
 	BLE	loopEnd
 
-	MOVW	8(R5), R0
-	MOVW	12(R5), R1
+	MOVM.IA.W	(R8), [R0, R1, R2, R3, R4, R5]
 	EOR	$(1<<31), R1
-	MOVW	R1, 8(R5)
-	MOVW	R0, 12(R5)
+	EOR	$(1<<31), R2
+	EOR	$(1<<31), R3
+	EOR	$(1<<31), R4
+	MOVM.DA.W	[R1, R0, R2, R3, R5, R4], (R8)
+	ADD	$32, R8
 
-	MOVW	16(R5), R0
-	MOVW	20(R5), R1
-	EOR	$(1<<31), R0
-	EOR	$(1<<31), R1
-	MOVW	R0, 16(R5)
-	MOVW	R1, 20(R5)
+	// MOVW	8(R8), R0
+	// MOVW	12(R8), R1
+	// EOR	$(1<<31), R1
+	// MOVW	R1, 8(R8)
+	// MOVW	R0, 12(R8)
 
-	MOVW	24(R5), R0
-	MOVW	28(R5), R1
-	EOR	$(1<<31), R0
-	MOVW	R1, 24(R5)
-	MOVW	R0, 28(R5)
+	// MOVW	16(R8), R0
+	// MOVW	20(R8), R1
+	// EOR	$(1<<31), R0
+	// EOR	$(1<<31), R1
+	// MOVW	R0, 16(R8)
+	// MOVW	R1, 20(R8)
 
-	ADD	$32, R5
+	// MOVW	24(R8), R0
+	// MOVW	28(R8), R1
+	// EOR	$(1<<31), R0
+	// MOVW	R1, 24(R8)
+	// MOVW	R0, 28(R8)
+
+	// ADD	$32, R8
 
 	B	loop
 loopEnd:
