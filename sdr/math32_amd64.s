8c9066a057824c4930d0515280d62f133e5ac1f9
Samuel Stauffer <samuelks@twitter.com>
2013-08-09 18:08:36 -0700

    Optimizations

--- sdr/math32_amd64.s
+++ sdr/math32_amd64.s
@@ -1,51 +1,45 @@
 
 TEXT ·FastAtan2(SB),7,$0
-	MOVSS	y+0(FP), X5
+	MOVSS	y+0(FP), X1
 	MOVSS	x+4(FP), X4
 
-	// abs(y)
 	MOVQ	$(1<<31), BX
-	MOVQ	BX, X2
-	MOVSS	X2, X7
-	ANDNPS	X5, X2
+	MOVQ	BX, X3
+	MOVSS	X3, X5
 
-	MOVSS	$1.0e-10, X3
-	ADDSS	X2, X3
-
-	MOVSS	$0.0, X6
-	UCOMISS	X4, X6
-	JLO	L4
+	// abs(y) + 1.0e-10
+	ANDNPS	X1, X3
+	ADDSS	$1.0e-10, X3
 
 	MOVSS	X4, X2
+
+	MOVSS	$0.0, X0
+	UCOMISS	X0, X4
+	JHI	L4
+
 	ADDSS	X3, X2	// abs(y) + x
-	MOVSS	X3, X1
-	SUBSS	X4, X1	// abs(y) - x
+	MOVSS	X3, X0
+	SUBSS	X4, X0	// abs(y) - x
+	DIVSS	X0, X2
 	MOVSS	$2.356194496154785, X3
 	JMP	L5
 L4:
-	MOVSS	X4, X2
 	SUBSS	X3, X2	// x - abs(y)
-	MOVSS	X4, X1
-	ADDSS	X3, X1	// x + abs(y)
+	ADDSS	X3, X4	// x + abs(y)
+	DIVSS	X4, X2
 	MOVSS	$0.7853981852531433, X3
 L5:
 
-	DIVSS	X1, X2
 	MOVSS	$0.1963, X0
 	MULSS	X2, X0
 	MULSS	X2, X0
-	MOVSS	$0.9817, X1
-	SUBSS	X1, X0
+	SUBSS	$0.9817, X0
 	MULSS	X2, X0
-	MOVSS	X3, X2
-	ADDSS	X0, X2
+	ADDSS	X3, X0
 
-	UCOMISS	X5, X6
-	JLO	L7
+	// if x < 0: -angle
+	ANDPS	X1, X5
+	XORPS	X5, X0
 
-	// -angle
-	XORPS	X7, X2
-
-L7:
-	MOVSS	X2, ret+8(FP)
+	MOVSS	X0, ret+8(FP)
 	RET
