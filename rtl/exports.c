194e20ee8621d4a1d2796a3610910fbb09a278cc
Samuel Stauffer <samuel@descolada.com>
2015-06-09 21:47:53 -0700

    Move rtl-sdr to another package

--- rtl/exports.c
+++
@@ -1,7 +0,0 @@
-#include "exports.h"
-
-void cbAsync(unsigned char *buf, uint32_t len, void *ctx) {
-	cbAsyncGo(buf, len, ctx);
-}
-
-const rtlsdr_read_async_cb_t *cbAsyncPtr = (rtlsdr_read_async_cb_t*)&cbAsync;
